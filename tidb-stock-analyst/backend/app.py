b94922950ae91712d7c3af893562ff8444377744
TN0123 <tanaynaik149@gmail.com>
2024-08-21 10:08:46 -0400

    implemented writing to database

--- tidb-stock-analyst/backend/app.py
+++ tidb-stock-analyst/backend/app.py
@@ -2,8 +2,6 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
 import subprocess
 import json
-import os
-import logging
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}}) 
@@ -31,16 +29,12 @@ def run_script():
 def scrape_data():
     result = subprocess.run(['node', '../scripts/scrape.js'], capture_output=True, text=True)
     articles = result.stdout
-    logging.info(articles)
-
-    # env = os.environ.copy()
-    # env['PYTHONPATH'] = 'C:/Users/Tanay N/AppData/Roaming/Python/Python312/site-packages;C:/Python312/Lib/site-packages;'
 
     python_path = "C/Python312/python.exe"
     output = subprocess.run(['python', '../scripts/processData.py', articles], capture_output=True, text=True)
    
-    return jsonify({'stdout' : output.stdout, 'stderr': output.stderr})
-    # return jsonify(json.loads(articles))
+    # return jsonify({'stdout' : output.stdout, 'stderr': output.stderr})
+    return jsonify(json.loads(articles))
 
 
 if __name__ == '__main__':
